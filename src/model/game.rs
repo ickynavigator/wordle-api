947ea4354337e044c79b922454b89e201e740aa9
Obi Fortune <gabrielobi.of@gmail.com>
2022-10-10 20:58:43 +0100

    Update game model and add implementations

--- src/model/game.rs
+++ src/model/game.rs
@@ -1,7 +1,36 @@
+use chrono::Utc;
 use tide::prelude::*;
+use uuid::Uuid;
 
+/// The game model for the wordle session.
 #[derive(Debug, Deserialize)]
 pub struct Game {
-    /// the game's creator
-    pub creator_id: u64,
+    /// Game ID
+    pub id: String,
+    /// Game's creator
+    pub creator_id: String,
+    /// Tries the user made
+    pub attempts: Vec<String>,
+    /// Time the game started
+    pub start_time: i64,
+}
+
+impl Game {
+    /// Create a new game session with a given creator ID.
+    /// Uses the timestamp to generate a start time
+    pub fn new_game(creator_id: String) -> Game {
+        let timestamp = Utc::now().timestamp();
+
+        Game {
+            id: Uuid::new_v4().to_string(),
+            creator_id,
+            attempts: vec![],
+            start_time: timestamp,
+        }
+    }
+
+    /// Push a new attempt on the game session
+    pub fn add_attempt(&mut self, attempt: String) {
+        self.attempts.push(attempt);
+    }
 }
